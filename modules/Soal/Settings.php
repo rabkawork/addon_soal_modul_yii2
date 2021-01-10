0026a403fa0963bfa025b45f14d295043ce13d4f
Ahadian Akbar <rabkawork@gmail.com>
2021-01-10 21:14:57 +0700

    modules

---
+++ modules/Soal/Settings.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\modules\Soal;
+
+/**
+ * Settings module definition class
+ */
+class Settings extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'app\modules\Soal\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
