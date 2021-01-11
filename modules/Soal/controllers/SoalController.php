4032a4dbe1cc7297cd505546e7ab840025cbbefc
Ahadian Akbar <=>
2021-01-11 18:15:27 +0700

    add soal controller

---
+++ modules/Soal/controllers/SoalController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace app\modules\Soal\controllers;
+
+use yii\web\Controller;
+
+/**
+ * Default controller for the `Settings` module
+ */
+class SoalController extends Controller
+{
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+        // return $this->render('index');
+    }
+
+
+    
+}
