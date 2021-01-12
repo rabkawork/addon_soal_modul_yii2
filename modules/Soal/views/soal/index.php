d86f094eaaed386a5ba8a3e142126addca2981c0
Ahadian Akbar <=>
2021-01-12 22:21:40 +0700

    update soal index

--- modules/Soal/views/soal/index.php
+++ modules/Soal/views/soal/index.php
@@ -1,9 +1,20 @@
+<a href="" class="btn btn-primary"> Tambah Jurusan </a>
+
 <table class="table table-striped">
     <thead>
       <tr>
-        <th>Firstname</th>
-        <th>Lastname</th>
-        <th>Email</th>
+        <th>Judul</th>
+        <th>Jumlah</th>
+        <th>Mata Pelajaran</th>
+        <th>Kelas/Jurusan</th>
+        <th>Jenjang</th>
+        <th>Kurikulum</th>
+        <th>Tahun Ajaran</th>
+        <th>Tanggal Input</th>
+        <th>Status</th>
+        <th>Creator</th>
+        <th>Harga</th>
+        <th>Aksi</th>
       </tr>
     </thead>
     <tbody>
@@ -11,16 +22,6 @@
         <td>John</td>
         <td>Doe</td>
         <td>john@example.com</td>
-      </tr>
-      <tr>
-        <td>Mary</td>
-        <td>Moe</td>
-        <td>mary@example.com</td>
-      </tr>
-      <tr>
-        <td>July</td>
-        <td>Dooley</td>
-        <td>july@example.com</td>
       </tr>
     </tbody>
   </table>
